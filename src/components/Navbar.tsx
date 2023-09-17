cbf6eafb90fa9e7c91ab680aff7b77ea29b09865
ratishjain <ratishjain6@gmail.com>
2023-09-17 23:56:03 +0530

    added link to logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,9 @@ function Navbar() {
   return (
     <Popover className=" mx-auto flex items-center justify-around border-b-2 px-6 py-2 h-16 text-black grow bg-white">
       <h1 className=" font-bold ">
-        <Image src={logo} alt="logo" className="w-[100px]" />
+        <Link href="/">
+          <Image src={logo} alt="logo" className="w-[100px]" />
+        </Link>
       </h1>
       <div className=" grow  md:grow-0">
         <div className=" hidden sm:flex items-center justify-center gap-2 md:gap-8">
