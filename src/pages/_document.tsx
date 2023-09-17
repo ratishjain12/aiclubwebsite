c490c57f4e30c476631c3892f5e10140407b8240
ratishjain <ratishjain6@gmail.com>
2023-09-17 23:48:40 +0530

    footer changes done

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,8 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en" style={{ scrollBehavior: "smooth" }}>
-      <Head title="AI Club" />
-
+      <Head />
       <body>
         <Main />
         <NextScript />
