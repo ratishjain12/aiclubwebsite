ea2a5c089c0bc9af7d7e66ad15799d2c34fa6d99
ratishjain <ratishjain6@gmail.com>
2023-09-18 00:41:03 +0530

    done with changes

--- src/components/Starter.tsx
+++ src/components/Starter.tsx
@@ -31,35 +31,36 @@ export default function Start() {
           CLUB, where we redefine the future of intelligence together!
         </p>
       </section>
-
-      <section
-        id="vision"
-        className="mt-12 bg-[#82A0D8] md:h-[500px] flex justify-center items-center flex-col rounded-md mx-4"
-      >
-        <h2 className="text-3xl text-center md:text-4xl  font-bold bg-white rounded-lg md:w-[400px] min-w-[280px] overflow-y-hidden mt-3">
-          Vision
-        </h2>
-        <p className="text-md w-[83vw]  md:w-[90vw] mx-auto md:text-2xl text-white mt-4 p-3 ">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam iure
-          expedita voluptatum cupiditate quaerat maiores laboriosam porro est
-          doloribus maxime eligendi tempore a animi, assumenda quis praesentium
-          minima vitae nemo.
-        </p>
-      </section>
-      <section
-        id="mission"
-        className="mt-12 bg-[#088395]  md:h-[500px] flex justify-center items-center flex-col  mx-4 rounded-md "
-      >
-        <h2 className="text-3xl text-center md:text-4xl  font-bold bg-white rounded-lg md:w-[400px] min-w-[280px] overflow-y-hidden mt-3">
-          Mission
-        </h2>
-        <p className="text-md w-[83vw]  md:w-[90vw] mx-auto md:text-2xl text-white mt-4 p-3">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam iure
-          expedita voluptatum cupiditate quaerat maiores laboriosam porro est
-          doloribus maxime eligendi tempore a animi, assumenda quis praesentium
-          minima vitae nemo.
-        </p>
-      </section>
+      <div className="bg-[#EDE4FF] border-t-4  border-pink-500 p-6 ">
+        <section
+          id="vision"
+          className=" mt-6 bg-[#713ABE] md:h-[500px] flex justify-center items-center flex-col rounded-md mx-4 shadow-lg"
+        >
+          <h2 className="text-3xl text-center md:text-4xl  font-bold bg-white rounded-lg md:w-[400px] min-w-[280px] overflow-y-hidden mt-3">
+            Vision
+          </h2>
+          <p className="text-md w-[83vw]  md:w-[90vw] mx-auto md:text-2xl text-white md:text-center mt-4 p-3 ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam iure
+            expedita voluptatum cupiditate quaerat maiores laboriosam porro est
+            doloribus maxime eligendi tempore a animi, assumenda quis
+            praesentium minima vitae nemo.
+          </p>
+        </section>
+        <section
+          id="mission"
+          className="mt-12 bg-purple-400  md:h-[500px] flex justify-center items-center flex-col  mx-4 rounded-md shadow-md"
+        >
+          <h2 className="text-3xl text-center md:text-4xl  font-bold bg-white rounded-lg md:w-[400px] min-w-[280px] overflow-y-hidden mt-3">
+            Mission
+          </h2>
+          <p className="text-md w-[83vw]  md:w-[90vw] mx-auto md:text-2xl text-white mt-4 p-3 md:text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam iure
+            expedita voluptatum cupiditate quaerat maiores laboriosam porro est
+            doloribus maxime eligendi tempore a animi, assumenda quis
+            praesentium minima vitae nemo.
+          </p>
+        </section>
+      </div>
       <section id="team" className="mt-12">
         <h2 className="text-center text-2xl md:text-4xl  mt-12 mb-10">
           Meet Our Core Team Members
@@ -70,6 +71,7 @@ export default function Start() {
           })}
         </div>
       </section>
+
       <section
         id="contact"
         className="mt-12 h-[300px] w-full bg-black border-t-4 border-pink-500 flex flex-col justify-center items-center "
