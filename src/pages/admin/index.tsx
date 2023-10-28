f28b048ea52df8f0e1f03d7c0088121654e52c18
ratishjain <ratishjain6@gmail.com>
2023-10-28 15:38:07 +0530

    error message fix

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -60,7 +60,7 @@ export default function Login() {
               onChange={(e) => setPassword(e.target.value)}
               className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
             />
-            <p className="text-red-500 mt-1">*{error}*</p>
+            {error && <p className="text-red-500 mt-1">*{error}*</p>}
           </div>
           <div className="mt-6">
             <button
