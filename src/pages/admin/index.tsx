9bc17953efd4f17d83ee62d59c6bf5fa2b5464dd
Jay Patel <79038529+Jay24004@users.noreply.github.com>
2023-10-29 11:29:39 +0530

    add use of .env for admin pass/user insted of hardcod

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,6 +1,9 @@
-"use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, use } from "react";
 import { useRouter } from "next/router";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -13,26 +16,31 @@ export default function Login() {
     }
   }, []);
 
-  const adminSignIn = (e: any): void => {
+  const adminSignIn = (e: React.FormEvent<HTMLFormElement>): void => {
+    const admin_username = process.env.ADMIN_USERNAME;
+    const admin_password = process.env.ADMIN_PASSWORD;
+
     e.preventDefault();
     if (username === "" || password === "") {
       setError("Please fill in all the credentials");
       return;
     }
-    if (username === "admin" && password === "contentteam") {
-      localStorage.setItem("admin", "true");
-      router.push("/createletter");
-    } else {
-      alert("Invalid username or password!");
-    }
+
+      if (String(username) === String(admin_username) && String(password) === String(admin_password)) {
+        localStorage.setItem("admin", "true");
+        router.push("/createletter");
+      } else {
+        alert("Invalid username or password!");
+      }
   };
+
   return (
     <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
       <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
         <h1 className="text-3xl font-semibold text-center text-grey-700 ">
           Admin
         </h1>
-        <form className="mt-6">
+        <form className="mt-6" onSubmit={adminSignIn}>
           <div className="mb-2">
             <label
               htmlFor="Username"
@@ -60,12 +68,12 @@ export default function Login() {
               onChange={(e) => setPassword(e.target.value)}
               className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
             />
-            {error && <p className="text-red-500 mt-1">*{error}*</p>}
+            {error && <p className="text-red-500 mt-1">{error}</p>}
           </div>
           <div className="mt-6">
             <button
+              type="submit"
               className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600"
-              onClick={adminSignIn}
             >
               Login
             </button>
